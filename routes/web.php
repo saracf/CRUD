e844adf5ad21d37e3d6c8593f261364c1f77aa76
saracf <saracnhha1@gmail.com>
2021-08-10 17:16:30 -0500

    22:07

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-
-
-Route: get(function () {
-return view(view'welcome');
-},
-    '');
-Route:: get ('listagem-usuario', 'UserControllerlistUser');
-Route::verb('uri', 'Contoler@,method')->name('route_name');
-Route::get('usuarios', 'Form\\TestContoller@listAllUsers')->('usuaio')
-
+Route::group(['middleware'=>  'web'], function(){
+    Route::get('/', 'homecontroller@index');
+    Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+Route::get('/usuarios', 'UsuariosController@index');
 
 
